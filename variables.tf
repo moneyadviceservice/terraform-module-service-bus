672b18f6a6c9ad63f7e52d6ed11dccb4ef178482
Zoe Cope <zoe.cope@hmcts.net>
2025-02-20 16:07:58 +0000

    amend type

--- variables.tf
+++ variables.tf
@@ -111,7 +111,7 @@ variable "trusted_services_allowed" {
 }
 
 variable "ip_rules" {
-  type        = string
+  type        = list
   description = "One or more IP Addresses, or CIDR Blocks which should be able to access the ServiceBus Namespace."
   default     = [""]
 }
