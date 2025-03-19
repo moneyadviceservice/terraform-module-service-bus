7a31bf8ac6c762b66bee3b70921bdf55b588f2b8
Zoe Cope <zoe.cope@hmcts.net>
2025-03-19 12:05:58 +0000

    make network rule set dynamic

--- main.tf
+++ main.tf
@@ -1,26 +1,29 @@
 resource "azurerm_servicebus_namespace" "this" {
-  name                          = var.name == null ? "${var.product}-sbns-${var.env}" : "${var.name}-${var.env}"
-  location                      = var.location
-  resource_group_name           = var.resource_group_name
-  sku                           = var.sku
-  capacity                      = var.capacity
-  premium_messaging_partitions  = var.premium_messaging_partitions
-  public_network_access_enabled = var.public_network_access_enabled
+  name                         = var.name == null ? "${var.product}-sbns-${var.env}" : "${var.name}-${var.env}"
+  location                     = var.location
+  resource_group_name          = var.resource_group_name
+  sku                          = var.sku
+  capacity                     = var.capacity
+  premium_messaging_partitions = var.premium_messaging_partitions
 
-  network_rule_set {
-    default_action                = var.network_rule_set_default_action
-    public_network_access_enabled = var.public_network_access_enabled
-    trusted_services_allowed      = var.trusted_services_allowed
-    ip_rules                      = var.ip_rules
-    dynamic "network_rules" {
-      for_each = var.subnet_id != null ? [1] : []
-      content {
-        subnet_id = var.subnet_id
+  dynamic "network_rule_set" {
+    for_each = var.ip_rules != null ? [1] : []
+    content {
+      default_action                = var.network_rule_set_default_action
+      public_network_access_enabled = var.public_network_access_enabled
+      trusted_services_allowed      = var.trusted_services_allowed
+      ip_rules                      = var.ip_rules
+    }
+  }
+  dynamic "network_rules" {
+    for_each = var.subnet_id != null ? [1] : []
+    content {
+      subnet_id = var.subnet_id
 
-      }
     }
   }
 }
+
 
 resource "azurerm_servicebus_queue" "this" {
   for_each     = var.queues
