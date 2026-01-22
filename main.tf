8fd5ac49d0b5aa3501ba58ad711d280cc1d35cc3
Zoe Cope <zoe.cope@hmcts.net>
2026-01-22 12:07:02 +0000

    refactor

--- main.tf
+++ main.tf
@@ -27,13 +27,10 @@ resource "azurerm_servicebus_queue" "this" {
   name         = each.value.queue_name
   namespace_id = azurerm_servicebus_namespace.this.id
 
-  partitioning_enabled = var.enable_partitioning
-  auto_delete_on_idle  = var.auto_delete_on_idle
-  default_message_ttl  = var.default_message_ttl
-  lock_duration = coalesce(
-    each.value.queue_lock_duration, null,
-    var.lock_duration
-  )
+  partitioning_enabled  = var.enable_partitioning
+  auto_delete_on_idle   = var.auto_delete_on_idle
+  default_message_ttl   = var.default_message_ttl
+  lock_duration         = coalesce(each.value.queue_lock_duration, var.lock_duration)
   max_delivery_count    = var.max_delivery_count
   max_size_in_megabytes = var.max_size_in_megabytes
 }
