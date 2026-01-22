7e216c3d27953791dc9f381aafe4e340a4fdd742
Zoe Cope <zoe.cope@hmcts.net>
2026-01-22 11:27:06 +0000

    make optional

--- main.tf
+++ main.tf
@@ -27,10 +27,13 @@ resource "azurerm_servicebus_queue" "this" {
   name         = each.value.queue_name
   namespace_id = azurerm_servicebus_namespace.this.id
 
-  partitioning_enabled  = var.enable_partitioning
-  auto_delete_on_idle   = var.auto_delete_on_idle
-  default_message_ttl   = var.default_message_ttl
-  lock_duration         = each.value.queue_lock_duration
+  partitioning_enabled = var.enable_partitioning
+  auto_delete_on_idle  = var.auto_delete_on_idle
+  default_message_ttl  = var.default_message_ttl
+  lock_duration = try(
+    each.value.queue_lock_duration,
+    var.lock_duration
+  )
   max_delivery_count    = var.max_delivery_count
   max_size_in_megabytes = var.max_size_in_megabytes
 }
--- variables.tf
+++ variables.tf
@@ -27,11 +27,12 @@ variable "product" {
 variable "queues" {
   type = map(object({
     queue_name          = string
-    queue_lock_duration = string
+    queue_lock_duration = optional(string)
   }))
   description = "Map of service bus queues to create."
   default     = {}
 }
+
 variable "default_message_ttl" {
   type        = string
   description = "Optional) The ISO 8601 timespan duration of the TTL of messages sent to this queue."
